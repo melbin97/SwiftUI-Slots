0348dd32cb6181b77cfd98ac2bd0ea8ba6bfc8d5
Melbin Mathew <mmathew@trl.co.uk>
2022-01-07 21:37:17 +0530

    Initial Commit

---
+++ SwiftUI-Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI-Slots
+//
+//  Created by Melbin Mathew on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
